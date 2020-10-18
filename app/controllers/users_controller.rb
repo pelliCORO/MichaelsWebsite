0faf2b110ea1ca42318d2f20805fc977ca2d4fc1
michael <michael@pellicoro.eu>
2020-10-18 11:20:14 +0200

    signup page/edit password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,8 +35,8 @@ class UsersController < ApplicationController
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { redirect_to :signup}
+        #format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
